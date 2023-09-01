698db43fb6d061a6f5972f25697fdf3525d2f0e3
Eruviel Garcia <131491666+EruvielGS@users.noreply.github.com>
2023-09-01 00:22:42 -0700

    UPDATE FRONT

--- Frontend/entrada.js
+++ Frontend/entrada.js
@@ -1,40 +1,71 @@
-const llegada = document.getElementById('formularioLlegada');
+// Declarar pads
+let vigilantePad2, usuarioPad2;
 
-llegada.addEventListener('submit', function(e) {
+function initPads2() {
+    vigilantePad2 = new SignaturePad(document.getElementById("vigilanteCanvas2"));
+    usuarioPad2 = new SignaturePad(document.getElementById("usuarioCanvas2"));
+}
 
-    e.preventDefault(); // previene que se recargue la página
-  
-    // obtener datos del formulario
-    const data = {
-      fechaLlegada: document.getElementById('fechaLlegada').value,
-      departamento: document.getElementById('departamento').value,
-      vehiculo: document.getElementById('vehiculo').value,
-      horaLlegada: document.getElementById('horaLlegada').value,
-      kmLlegada: document.getElementById('kmLlegada').value,
-      nombreVigilante2: document.getElementById('nombreVigilante2').value,
-      firmaVigilante2: document.getElementById('firmaVigilante2').value,
-      firmaUsuario2: document.getElementById('firmaUsuario2').value,
-      destinoLocal: document.getElementById('destinoLocal').checked,
-      destinoForaneo: document.getElementById('destinoForaneo').checked,
-      accesorios: Array.from(document.querySelectorAll('input[name="accesorios"]:checked')).map(input => input.value),
-      observaciones: document.getElementById('observaciones').value
-    };
-    
-    console.log(data);
-  
-    // enviar datos a API
-    fetch('http://localhost:3010/entry', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      console.error(error); 
+function clearPad2(pad) {
+    pad.clear();
+}
+
+// Eventos
+document.addEventListener("DOMContentLoaded", initPads2);
+
+document.querySelectorAll(".btn-clear").forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const padName = btn.getAttribute("data-pad");
+        const pad = padName === "vigilantePad2" ? vigilantePad2 : usuarioPad2;
+        clearPad2(pad);
     });
-  
-  });
+});
+
+// Obtener los datos de las firmas en formato base64
+function getSignatureData2(pad) {
+    return pad.toDataURL();
+}
+
+function submitForm2(e) {
+    e.preventDefault();
+
+    const vigilanteSignature2 = getSignatureData2(vigilantePad2);
+    const usuarioSignature2 = getSignatureData2(usuarioPad2);
+
+    const data = {
+        fechaLlegada: document.getElementById("fechaLlegada").value,
+        departamento: document.getElementById("departamento").value,
+        vehiculo: document.getElementById("vehiculo").value,
+        horaLlegada: document.getElementById("horaLlegada").value,
+        kmLlegada: document.getElementById("kmLlegada").value,
+        nombreVigilante2: document.getElementById("nombreVigilante2").value,
+        firmaVigilante2: vigilanteSignature2,
+        firmaUsuario2: usuarioSignature2,
+        destinoLocal: document.getElementById("destinoLocal").checked,
+        destinoForaneo: document.getElementById("destinoForaneo").checked,
+        accesorios: Array.from(document.querySelectorAll('input[name="accesorios"]:checked')).map(checkbox => checkbox.value),
+        observaciones: document.getElementById("observaciones").value
+    };
+
+    console.log(data);
+
+    //Enviar datos a la API o realizar otras acciones aquí
+    //Por ejemplo:
+    fetch("http://localhost:3010/arrival", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+const llegada = document.getElementById("formulario");
+
+llegada.addEventListener("submit", submitForm2);
--- Frontend/salida.js
+++ Frontend/salida.js
@@ -2,77 +2,65 @@
 let vigilantePad, usuarioPad;
 
 function initPads() {
-  vigilantePad = new SignaturePad(document.getElementById("vigilanteCanvas"));
-  usuarioPad = new SignaturePad(document.getElementById("usuarioCanvas"));
+    vigilantePad = new SignaturePad(document.getElementById("vigilanteCanvas"));
+    usuarioPad = new SignaturePad(document.getElementById("usuarioCanvas"));
 }
 
 function clearPad(pad) {
-  pad.clear();
+    pad.clear();
 }
 
 // Eventos
 document.addEventListener("DOMContentLoaded", initPads);
 
-document.querySelectorAll(".btn-clear").forEach((btn, index) => {
-  btn.addEventListener("click", () =>
-    clearPad(index === 0 ? vigilantePad : usuarioPad)
-  );
+document.querySelectorAll(".btn-clear").forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const padName = btn.getAttribute("data-pad");
+        const pad = padName === "vigilantePad" ? vigilantePad : usuarioPad;
+        clearPad(pad);
+    });
 });
 
 // Obtener los datos de las firmas en formato base64
 function getSignatureData(pad) {
-  return pad.toDataURL(); // Obtiene la firma en base64
+    return pad.toDataURL();
 }
+
 function submitForm(e) {
-  e.preventDefault(); // Evitar el envío del formulario por defecto
+    e.preventDefault();
 
-  const vigilanteSignature = getSignatureData(vigilantePad);
-  const usuarioSignature = getSignatureData(usuarioPad);
+    const vigilanteSignature = getSignatureData(vigilantePad);
+    const usuarioSignature = getSignatureData(usuarioPad);
 
-  // Aquí puedes usar las firmas en formato base64 como desees, por ejemplo, enviarlas al servidor
-  return {
-    vigilanteSignature,
-    usuarioSignature,
-  };
+    const data = {
+        folio: document.getElementById("folio").value,
+        fechaSalida: document.getElementById("fechaSalida").value,
+        nombre: document.getElementById("nombre").value,
+        placas: document.getElementById("placas").value,
+        horaSalida: document.getElementById("horaSalida").value,
+        kmSalida: document.getElementById("kmSalida").value,
+        nombreVigilante: document.getElementById("nombreVigilante").value,
+        firmaVigilante: vigilanteSignature,
+        firmaUsuario: usuarioSignature,
+    };
 
-  // Resto de la lógica para enviar los datos del formulario si es necesario
-}
+    console.log(data);
 
-
-const salida = document.getElementById("formularioSalida");
-
-salida.addEventListener("submit", function (e) {
-  e.preventDefault(); // previene que se recargue la página
-  const firmas = submitForm(e);
-  // obtener datos del formulario
-  const data = {
-    folio: document.getElementById("folio").value,
-    fechaSalida: document.getElementById("fechaSalida").value,
-    nombre: document.getElementById("nombre").value,
-    placas: document.getElementById("placas").value,
-    horaSalida: document.getElementById("horaSalida").value,
-    kmSalida: document.getElementById("kmSalida").value,
-    nombreVigilante: document.getElementById("nombreVigilante").value,
-    firmaVigilante: firmas.vigilanteSignature,
-    firmaUsuario: firmas.usuarioSignature,
-  };
-
-  console.log(data);
-
-  // enviar datos a API
-  fetch("http://localhost:3010/exit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
+    fetch("http://localhost:3010/exit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
     .then((response) => {
-      console.log(response);
+        console.log(response);
     })
     .catch((error) => {
-      console.error(error);
+        console.error(error);
     });
-});
+}
 
+const salida = document.getElementById("formulario");
 
+salida.addEventListener("submit", submitForm);
