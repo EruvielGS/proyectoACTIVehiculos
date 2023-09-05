556c0a2b15a632ecddf7103e4b29a450924daa41
Eruviel Garcia <eruvielgs@gmail.com>
2023-09-05 12:37:41 -0700

    Correccion de automovil

--- Backend/src/services/entryServices.ts
+++ Backend/src/services/entryServices.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from "express";
-import fs from "fs";
-import path from "path";
 
-//const coordenadas: any = [{"x":100,"y":150},{"x":250,"y":300}, {"x":350,"y":200}];
-//let coordenadas:any;
+
+let corde: any;
+// let coordenadas:any;
 
 const getEntrada = (req: Request, res: Response) => {
-  //console.log(coordenadas);
-  //return coordenadas;
+  console.log(corde);
+  return corde;
 };
 
 const postEntrada = (req: Request, res: Response) => {
- 
+  const {coordenadas} = req.body;
+  console.log(coordenadas);
+  corde = coordenadas;
+
+  return "se recibieron las coordenadas";
+  //return data;
 };
 
 export { getEntrada, postEntrada };
